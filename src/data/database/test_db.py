0953ec7675bc6ef8131b7eb235e1eef7321c4702
SethMurd <sethmurdock19@gmail.com>
2025-04-03 14:38:57 -0600

    testing fixes

--- src/data/database/test_db.py
+++ src/data/database/test_db.py
@@ -3,10 +3,10 @@ from crud import *
 
 if __name__ == "__main__":
     print_table_contents("student_profiles")
-    # print_table_contents("scenarios")
-    # print_table_contents("dialogues")
-    # print_table_contents("active_files")
-    # print_table_contents("inactive_files")
+    print_table_contents("scenarios")
+    print_table_contents("dialogues")
+    print_table_contents("active_files")
+    print_table_contents("inactive_files")
     print_table_contents("teacher_profiles")
     print(populate_teacher_profiles())
     print_table_contents("teacher_profiles")
